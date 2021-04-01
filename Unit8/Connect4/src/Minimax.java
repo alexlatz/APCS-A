04036260484d740e1e0ee6fdaa1c768d00ccb621
Alex Latz <alex@latz.tech>
2021-04-01 15:03:47 -0400

    minimax broken, out of time

--- Unit8/Connect4/src/Minimax.java
+++ Unit8/Connect4/src/Minimax.java
@@ -1,114 +1,97 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Minimax {
-    private static double weight3;
-    private static double weight2;
-    private static double weight1;
+    private static final double[] weight = {10.0, 100.0, 1000.0};
+    private Node node;
 
-    private class Node {
+    private class Node implements Comparable<Node> {
         private Board board;
-        private double heuristic;
+        private double value;
+        private ArrayList<Node> children;
 
         public Node(Board board) {
             this.board = board;
-            if (this.board.getWinner() == 0 && this.board.getMoves() < this.board.getCols()*this.board.getRows()) {
-                this.heuristic = Minimax.heuristic(board);
-            } else if (this.board.getWinner() == 2) this.heuristic = Double.POSITIVE_INFINITY;
-            else if (this.board.getWinner() == 1) this.heuristic = Double.NEGATIVE_INFINITY;
-            else this.heuristic = 0;
+            this.children = null;
         }
 
-        public Iterable<Node> getChildren() {
-            ArrayList<Node> list = new ArrayList<>();
-            for (int i = 0; i < board.getCols(); i++) {
-                if (board.getColHeight(i)+1 < board.getRows()) {
-                    Board child = board.copy();
-                    child.placeMarker(i, board.getMoves() % 2 == 0);
-                    list.add(new Node(child));
+        public ArrayList<Node> getChildren() {
+            if (this.children != null) return this.children;
+            else if (!isTerminal()) {
+                ArrayList<Node> list = new ArrayList<>();
+                for (int i = 0; i < board.getCols(); i++) {
+                    if (board.getColHeight(i)+1 < board.getRows()) {
+                        Board child = board.copy();
+                        child.placeMarker(i+1, board.getMoves() % 2 == 0);
+                        list.add(new Node(child));
+                    }
                 }
+                this.children = list;
             }
-            return list;
+            return null;
+        }
+
+        public boolean isTerminal() {
+            return !(board.getMoves() < board.getRows()*board.getCols() && board.getWinner() == 0);
+        }
+
+        @Override
+        public int compareTo(final Node o) {
+            return -Double.compare(this.value, o.value);
         }
     }
 
-    private static double getWeight(int i) {
-        switch (i) {
-            case 1:
-                return weight1;
-            case 2:
-                return weight2;
-            default:
-                return weight3;
+    public Minimax(Board start) {
+        this.node = new Node(start);
+        this.node.getChildren();
+    }
+
+    public int getMove(Board board) {
+        for (Node child : this.node.getChildren()) {
+            if (child.board.getLastMove() == board.getLastMove()) {
+                this.node = child;
+                break;
+            }
         }
+        alphaBeta(this.node, 10, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false);
+        this.node.getChildren();
+        Collections.sort(this.node.getChildren());
+        this.node = this.node.getChildren().get(0);
+        return this.node.board.getLastMove();
+    }
+
+    public double alphaBeta(Node node, int depth, double alpha, double beta, boolean maximizing) {
+        if (depth == 0 || node.isTerminal()) return heuristic(node.board);
+        if (maximizing) {
+            node.getChildren();
+            node.value = Double.NEGATIVE_INFINITY;
+            for (Node child : node.getChildren()) {
+                node.value = Math.max(node.value, alphaBeta(child, depth-1, alpha, beta, false));
+                alpha = Math.max(alpha, node.value);
+                if (alpha >= beta) break;
+            }
+        } else {
+            node.value = Double.POSITIVE_INFINITY;
+            node.getChildren();
+            for (Node child : node.getChildren()) {
+                node.value = Math.min(node.value, alphaBeta(child, depth-1, alpha, beta, true));
+                beta = Math.min(beta, node.value);
+                if (beta <= alpha) break;
+            }
+        }
+        return node.value;
     }
 
     private static double heuristic(Board board) {
-        int max = 0;
-        for (int i = 0; i < board.getCols(); i++) max = Math.max(board.getColHeight(i), max);
-        byte[][] arr = board.getBoard();
-        return horizontalHeuristic(arr, max) + verticalHeuristic(arr, board) + diagHeuristic(arr, max);
+        if (board.getWinner() == 0 && board.getMoves() < board.getCols()*board.getRows()) {
+            double heuristic = 0.0;
+            heuristic += board.numInRow(1) * Minimax.weight[0];
+            heuristic += board.numInRow(2) * Minimax.weight[1];
+            heuristic += board.numInRow(3) * Minimax.weight[2];
+            return heuristic;
+        } else if (board.getWinner() == 2) return 10000.0/board.getMoves();
+        else if (board.getWinner() == 1) return -10000.0/board.getMoves();
+        else return 0;
     }
 
-    //TODO: all of the heuristics bc they're bad
-    private static double horizontalHeuristic(byte[][] board, int max) {
-        double heuristic = 0.0;
-        for (int i = 0; i < max; i++) {
-            byte current = 0;
-            boolean lastSpace = false;
-            byte sum = 0;
-            for (int j = 0; j < board[0].length; j++) {
-                if (current == 0 && board[i][j] != current) current = board[i][j];
-                if (board[i][j] == current) sum++;
-                else if (board[i][j] != current && lastSpace) {
-                    if (board[i][j] == 2) heuristic += Minimax.getWeight(sum);
-                    else heuristic -= Minimax.getWeight(sum);
-                    sum = 0;
-                    lastSpace = false;
-                    current = board[i][j];
-                }
-                else lastSpace = true;
-            }
-        }
-        return heuristic;
-    }
-
-    private static double verticalHeuristic(byte[][] arr, Board board) {
-        double heuristic = 0.0;
-        for (int j = 0; j < board.getCols(); j++) {
-            byte sum = 0;
-            byte current = arr[0][j];
-            for (int i = 0; i < board.getColHeight(j); i++) {
-                if (arr[i][j] == current) sum++;
-                else {
-                    if (current == 2) heuristic += Minimax.getWeight(sum);
-                    else heuristic -= Minimax.getWeight(sum);
-                    current = arr[i][j];
-                    sum = 0;
-                }
-            }
-            if (current == 2) heuristic += Minimax.getWeight(sum);
-            else if (current == 1) heuristic -= Minimax.getWeight(sum);
-        }
-        return heuristic;
-    }
-
-    private static double diagHeuristic(byte[][] board, int max) {
-        //dont bother running if max < 4
-        //go thru each bottom left -> top right diag
-        //i = bottom of diag
-        //j = horizontal
-        if (max < 3) return 0.0;
-        for (int i = 0; i < max - 3 && i < board.length-3; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (j >= 3) {
-                    for (int k = 0; k <= 4; k++) {
-
-                    }
-                }
-                if (j <= board[0].length - 3) {
-
-                }
-            }
-        }
-    }
 }
